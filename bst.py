855275338cb1dd4627851d3a39d50b0e9b1312fd
cdevaraju <cdevaraju@sandvine.com>
2024-02-05 23:29:06 +0530

    Added traversal functions for bst

--- bst.py
+++ bst.py
@@ -1,5 +1,6 @@
 from tree_node import Node
 
+
 class BST:
     def __init__(self) -> None:
         self.head = None
@@ -9,7 +10,7 @@ class BST:
             self.head = Node(element)
             return True
         
-        temp =  self.head
+        temp = self.head
         while temp:
             if element < temp.value:
                 if not temp.left:
@@ -21,12 +22,33 @@ class BST:
                     temp.right = Node(element)
                     return True
                 temp = temp.right
+    
+    def pre_order_traversal(self, node):
+        temp = node
+        if temp:
+            print(temp.value)
+            self.pre_order_traversal(temp.left)
+            self.pre_order_traversal(temp.right)
+        
+    def post_order_traversal(self, node):
+        temp = node
+        if temp:
+            self.pre_order_traversal(temp.left)
+            self.pre_order_traversal(temp.right)
+            print(temp.value)
+
+    def in_order_traversal(self, node):
+        temp = node
+        if temp:
+            self.pre_order_traversal(temp.left)
+            print(temp.value)
+            self.pre_order_traversal(temp.right)
 
 
 def main():
     # Creating a Binary Search Tree object
     bst = BST()
-    
+
     # Adding nodes to the Binary Search Tree
     bst.add_node(50)
     bst.add_node(30)
@@ -35,3 +57,19 @@ def main():
     bst.add_node(40)
     bst.add_node(60)
     bst.add_node(80)
+
+    # Pre-order Traversal
+    print("Pre-order Traversal:")
+    bst.pre_order_traversal(bst.head)
+
+    # In-order Traversal
+    print("\nIn-order Traversal:")
+    bst.in_order_traversal(bst.head)
+
+    # Post-order Traversal
+    print("\nPost-order Traversal:")
+    bst.post_order_traversal(bst.head)
+
+
+if __name__ == "__main__":
+    main()
