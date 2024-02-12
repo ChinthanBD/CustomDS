c817f72f2df793b61d856953eaac7b5177517d70
cdevaraju <cdevaraju@sandvine.com>
2024-02-12 15:21:10 +0530

    Heapify Bug fix

--- node.py
+++ node.py
@@ -1,6 +1,22 @@
 class Node:
-    def __init__(self, key, value, next_node=None, prev_node=None):
-        self.key = key
-        self.value = value
-        self.next_node = next_node
-        self.prev_node = prev_node
+  def __init__(self, value, left=None, right=None, children=None):
+    self.value = value
+    self.left = left
+    self.right = right
+    self.children = children
+
+def getValue(self):
+  return self.value
+
+def getChildren(self):
+  return self.children
+
+def addToChildren(self, value):
+  if isinstance(self.children, list):
+    self.children.append(value)
+    return True
+  elif isinstance(self.children, set):
+    self.children.add(value)
+    return True
+  else:
+    return False
