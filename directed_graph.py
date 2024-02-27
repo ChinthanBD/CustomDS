5cdf6d423078966bd7d445afd79e4107ba14f5aa
ChinthanBD <chinthan.bd@gmail.com>
2024-02-27 22:47:56 +0530

    Enum usage

--- directed_graph.py
+++ directed_graph.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+class TraversalType:
+    BF = 'BreadthFirst'
+    DF = 'DeapthFirst'
 
 class DirectedGraph:
     def __init__(self) -> None:
@@ -36,6 +39,23 @@ class DirectedGraph:
                         queue.append(neighbour)
         return traversal
 
+    def traversal(self, source, traversal_type = TraversalType.DF):
+        ds_for_traversal = [source] 
+        visited = set()
+        traversal = []
+        while ds_for_traversal:
+            vertex = ds_for_traversal.pop() if traversal_type == TraversalType.DF else ds_for_traversal.pop(0)
+            if vertex not in visited:
+                visited.add(vertex)
+                traversal.append(vertex)
+                adjacency_list = (reversed(self.adjacency_list[vertex]) 
+                                 if traversal_type == TraversalType.DF 
+                                 else self.adjacency_list[vertex])
+                for neighbour in adjacency_list:
+                    if neighbour not in visited:
+                        ds_for_traversal.append(neighbour)
+        return traversal
+    
     def has_path_stack_dfs(self, source, dest):
         stack = [(source, [source])]
         visited = set()
@@ -67,6 +87,23 @@ class DirectedGraph:
         print("No path from", source, "to", dest)
         return False
 
+    def has_path(self, source, dest, traversal_type=TraversalType.DF):
+        ds_for_traversal = [(source, [source])]
+        visited = set()
+        while ds_for_traversal:
+            vertex, path = ds_for_traversal.pop() if traversal_type == TraversalType.DF else ds_for_traversal.pop(0)
+            if vertex == dest:
+                print("Path from", source, "to", dest, ":", "->".join(map(str, path)))
+                return True
+            if vertex not in visited:
+                visited.add(vertex)
+                adjacency_list = (reversed(self.adjacency_list[vertex]) 
+                                 if traversal_type == TraversalType.DF 
+                                 else self.adjacency_list[vertex])                
+                for neighbour in adjacency_list:
+                    ds_for_traversal.append((neighbour, path + [neighbour]))
+        print("No path from", source, "to", dest)
+        return False
     
 def main():
     graph = DirectedGraph()
@@ -79,19 +116,30 @@ def main():
 
     # Depth First Search (DFS)
     print("DFS Traversal:", graph.dfs_traversal(0))
+    print("DFS Traversal:", graph.traversal(0,TraversalType.DF))
 
     # Breadth First Search (BFS)
     print("BFS Traversal:", graph.bfs_traversal(0))
+    print("BFS Traversal:", graph.traversal(0,TraversalType.BF))
 
     # Check if there is a path between two vertices using DFS
     print("Path between 0 and 3 exists (DFS)?", graph.has_path_stack_dfs(0, 3))
     print("Path between 0 and 4 exists (DFS)?", graph.has_path_stack_dfs(0, 4))
     print("Path between 5 and 4 exists (DFS)?", graph.has_path_stack_dfs(5, 4))
 
+    print("Path between 0 and 3 exists (DFS)?", graph.has_path(0, 3, TraversalType.DF))
+    print("Path between 0 and 4 exists (DFS)?", graph.has_path(0, 4, TraversalType.DF))
+    print("Path between 5 and 4 exists (DFS)?", graph.has_path(5, 4, TraversalType.DF))
+    
     # Check if there is a path between two vertices using BFS
     print("Path between 0 and 3 exists (BFS)?", graph.has_path_queue_bfs(0, 3))
     print("Path between 0 and 4 exists (BFS)?", graph.has_path_queue_bfs(0, 4))
     print("Path between 5 and 4 exists (BFS)?", graph.has_path_queue_bfs(5, 4))
+
+    print("Path between 0 and 3 exists (DFS)?", graph.has_path(0, 3, TraversalType.BF))
+    print("Path between 0 and 4 exists (DFS)?", graph.has_path(0, 4, TraversalType.BF))
+    print("Path between 5 and 4 exists (DFS)?", graph.has_path(5, 4, TraversalType.BF))
+    
 
 if __name__ == "__main__":
     main()
