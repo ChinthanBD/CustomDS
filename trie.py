d6478601836848ac38a09eb235282aa14dc2e0ee
Chinthan <chinthan.bd@gmail.com>
2024-03-19 23:07:53 +0530

    Trie implementation

---
+++ trie.py
@@ -0,0 +1,73 @@
+# https://www.codingninjas.com/studio/problems/implement-trie_631356?leftPanelTabValue=PROBLEM
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10**7)
+
+
+class Node:
+    def __init__(self):
+        self.links = [None] * 26
+        self.flag = False
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, string):
+        node = self.root
+        for char in string:
+            pos = ord(char) - ord('a')
+            if not node.links[pos]:
+                node.links[pos] = Node()
+            node = node.links[pos]
+        node.flag = True
+
+    
+    def search(self, word):
+        node = self.root
+        for char in word:
+            pos = ord(char) - ord('a')
+            if not node.links[pos]:
+                return False
+            node = node.links[pos]
+
+        if node.flag == False:
+            return False
+        return True
+
+        
+    def startWith(self, prefix):
+        node = self.root
+        for char in prefix:
+            pos = ord(char) - ord('a')
+            if not node.links[pos]:
+                return False
+            node = node.links[pos]
+        return True
+
+def main():
+    # Create a Trie instance
+    trie = Trie()
+
+    # Insert some words into the Trie
+    words = ["apple", "banana", "orange", "pear"]
+    for word in words:
+        trie.insert(word)
+
+    # Test search function
+    search_words = ["apple", "banana", "peach", "grape"]
+    for word in search_words:
+        if trie.search(word):
+            print(word, "is present in the Trie")
+        else:
+            print(word, "is not present in the Trie")
+
+    # Test startWith function
+    prefixes = ["app", "ban", "pea", "gr"]
+    for prefix in prefixes:
+        if trie.startWith(prefix):
+            print("Some word in the Trie starts with", prefix)
+        else:
+            print("No word in the Trie starts with", prefix)
+
+if __name__ == "__main__":
+    main()
